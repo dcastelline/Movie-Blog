11021e5dc4f8dea85b4c666feb98117a718fe041
coleb <cole.ballard97@gmail.com>
2021-02-16 14:30:44 -0600

    trying to get working version up

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const session = require("express-session");
 
 const PORT = process.env.PORT || 3306;
 
+// import { v4 as uuidv4 } from 'uuid';
+
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+// const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 const db = require("./models");
@@ -19,15 +21,21 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-const routes = require("./controllers/controller.js");
+// We need to use sessions to keep track of our user's login status
+// app.use(session({
+//   genid: function(req) {
+//     return uuidv4();
+//   }, 
+//   secret: process.env.SESSION_SECRET, 
+//   resave: true, 
+//   saveUninitialized: true 
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Requiring our routes
-app.use("/", routes);
+require('./controllers/controller.js')(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
