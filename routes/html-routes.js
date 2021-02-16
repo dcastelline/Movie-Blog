607b067f31e4dd9a60a75e44e949207e71360745
Daniel <daniel.castelline@gmail.com>
2021-02-16 09:55:08 -0600

    routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,17 @@
+// Dependencies
 const path = require('path');
 
 // Routes
 module.exports = (app) => {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads login.html
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../views/login.html'))
-  );
-
   // index route loads index.html
-  app.get('/index', (req, res) =>
+  app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../views/index.html'))
   );
 
+  // login route loads login.html
+  app.get('/login', (req, res) =>
+    res.sendFile(path.join(__dirname, '../views/login.html'))
+  );
 };
