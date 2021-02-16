8ead9d9cad28dc59a8846a98ab5ddbc732d03f90
coleb <cole.ballard97@gmail.com>
2021-02-16 13:20:01 -0600

    working on setting up server, and connecting to routes/controllers

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,15 +5,15 @@ async function getMovie() {
 		"headers": {
 			"x-rapidapi-key": "bc1026b9b5msh09f9bd72ecaf669p16f9e1jsn203e48375d38",
 			"x-rapidapi-host": "ott-details.p.rapidapi.com"
-		}
-	})
-		.then(response => response.json())
-		.then(response => {
-			console.log(response);
-		})
-		.catch(err => {
-			console.error(err);
-		});
+    }
+  })
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 }
 // platform information
 async function getPlatform() {
@@ -24,12 +24,12 @@ async function getPlatform() {
 			"x-rapidapi-host": "ott-details.p.rapidapi.com"
 		}
 	})
-		.then(response => {
-			console.log(response);
-		})
-		.catch(err => {
-			console.error(err);
-		});
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 }
 
 
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+
+const PORT = process.env.PORT || 3306;
+
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-const htmlRoutes = require("./routes/html-routes.js");
-const apiRoutes = require("./routes/api-routes.js");
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 3300;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
+
+// Serve static content
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
@@ -20,12 +24,14 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+const routes = require("./controllers/controller.js");
+
 // Requiring our routes
-htmlRoutes(app);
+app.use("/", routes);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
   });
 });
